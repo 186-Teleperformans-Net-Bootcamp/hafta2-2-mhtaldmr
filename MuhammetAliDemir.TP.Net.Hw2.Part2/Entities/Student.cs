b56d86d709b30866c1da204f1dc15e2bb08d31ef
mhtaldmr <mht.alidemir@gmail.com>
2022-05-28 14:29:44 +0300

    Adding Entity Classes.

---
+++ MuhammetAliDemir.TP.Net.Hw2.Part2/Entities/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace MuhammetAliDemir.TP.Net.Hw2.Part2.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MuhammetAliDemir.TP.Net.Hw2.Part2/Entities/Teacher.cs
@@ -0,0 +1,8 @@
+﻿namespace MuhammetAliDemir.TP.Net.Hw2.Part2.Entities
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
