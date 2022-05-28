cd90254314803114d08343d5ac41feb1a11f3cc0
mhtaldmr <mht.alidemir@gmail.com>
2022-05-28 15:13:26 +0300

    adding Dbsets.

--- MuhammetAliDemir.TP.Net.Hw2.Part2/Persistance/SchoolDBContext.cs
+++ MuhammetAliDemir.TP.Net.Hw2.Part2/Persistance/SchoolDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MuhammetAliDemir.TP.Net.Hw2.Part2.Entities;
 
 namespace MuhammetAliDemir.TP.Net.Hw2.Part2.Persistance
 {
@@ -8,6 +9,11 @@ namespace MuhammetAliDemir.TP.Net.Hw2.Part2.Persistance
         {
         }
 
+        public DbSet<School> Schools { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Course> Courses { get; set; }
 
     }
 }
